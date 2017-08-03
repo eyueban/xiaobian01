b34ec2dfb00e8fbbf40a642b0b2b2da99e7eb1bd
xiaobian2 <2931725715@qq.com>
2017-08-03 18:45:03 +0800

    ssasd

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3.
+ */
+public class HelloWorld {
+
+    @Test
+    public void helloWorld() {
+        System.out.println("HelloWorld");
+    }
+}
